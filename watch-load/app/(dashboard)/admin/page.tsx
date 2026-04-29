18d309fd2c5993344de2a9952048777f51a97b66
Maurice Lindner <31370550+Cobralion@users.noreply.github.com>
2026-04-29 13:20:03 +0200

    ui

--- watch-load/app/(dashboard)/admin/page.tsx
+++ watch-load/app/(dashboard)/admin/page.tsx
@@ -3,11 +3,11 @@ import AdminCreateUserCard from '@/components/admin/admin-create-user-card';
 
 export default async function Page() {
     return (
-        <>
-            <h1 className="text-2xl font-bold tracking-tight">Dashboard</h1>
+        <div className="flex items-start gap-6">
+            <h1 className="text-2xl font-bold tracking-tight">Admin Panel</h1>
             <div className="m-4 flex w-1/2 flex-col gap-6 pt-6">
-                <AdminCreateUserCard></AdminCreateUserCard>
+                <AdminCreateUserCard />
             </div>
-        </>
+        </div>
     );
 }
